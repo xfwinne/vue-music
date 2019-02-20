5109ad3b49ea97d20c690886e18b40cbd37c0cbe
xfwinne <782416218@qq.com>
2019-02-20 10:25:33 +0800

    modify slide

--- src/baseComponents/slider/slider.vue
+++ src/baseComponents/slider/slider.vue
@@ -1,170 +1,231 @@
 <!-- 轮播图组件（基础组件） -->
 <template>
-  <div class="slider" ref="slider">
-    <div class="slider-group" ref="sliderGroup">
+  <div class="slide" ref="slide">
+    <div class="slide-group" ref="slideGroup">
       <slot>
       </slot>
     </div>
-    <div class="dots">
-        <span class="dot" v-for="(item,index) in dots" :class="{active:currentPageIndex === index}"></span>
+    <div v-if="showDot" class="dots">
+    <!-- <div v-if="dots.length" class="dots"> -->
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
     </div>
   </div>
 </template>
 
-<script>
-  import {addClass} from 'common/js/dom.js'
+<script type="text/ecmascript-6">
+  import { addClass } from 'common/js/dom'
+  //import BScroll from 'scroll/index'
   import BScroll from 'better-scroll';
-
+  //const COMPONENT_NAME = 'slide'
   export default {
-    props:{
-        // 是否能循环轮播（默认能）
-        loop:{
-            type: Boolean,
-            default: true
-        },
-        // 是否自动轮播
-        autoPlay:{
-            type:Boolean,
-            default:true
-        },
-        // 几秒进行一次切换轮播
-        interval:{
-            type:Number,
-            default:4000
-        },
+   // name: COMPONENT_NAME,
+    props: {
+      loop: {
+        type: Boolean,
+        default: true
+      },
+      autoPlay: {
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      },
+      showDot: {
+        type: Boolean,
+        default: true
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      threshold: {
+        type: Number,
+        default: 0.3
+      },
+      speed: {
+        type: Number,
+        default: 400
+      }
     },
-    data(){
-        return{
-            // 轮播图中的点点
-            dots:[],
-            // 当前是哪张轮播图
-            currentPageIndex:0
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
+      }
+    },
+    mounted() {
+      this.update()
+      // 监听视图窗口大小发生变化就会渲染轮播图宽度
+      window.addEventListener('resize', () => {
+        if (!this.slide || !this.slide.enabled) {
+          return
         }
-        
-    },
-    mounted(){
-        // 为了让视图dom渲染好了才初始化better-scroll，这样不会报错,可以使用延迟函数，或者使用this.$nextTick
-        setTimeout(() => {
-            this._setSliderWidth();
-            this._initDots();
-            this._initSlider();
-            if(this.autoPlay){
-                this._autoplay();
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+          if (this.slide.isInTransition) {
+            this._onScrollEnd()
+          } else {
+            if (this.autoPlay) {
+              this._play()
             }
-
-
-        },20);
-
-        // 监听视图窗口大小发生变化就会渲染轮播图宽度
-        window.addEventListener('resize',() => {
-            // 如果slider没有初始化就什么都不做
-            if (!this.slider) {
-                return false;
-            }
-            // 重新渲染的时候不能再加两倍宽度了
-            this._setSliderWidth(true);
-            // 重新初始化better-scroll
-            this.slider.refresh();
-        })
-
-
+          }
+          this.refresh()
+        }, 60)
+      })
     },
-    destroyed(){
+    activated() {
+      if (!this.slide) {
+        return
+      }
+      this.slide.enable()
+      let pageIndex = this.slide.getCurrentPage().pageX
+      this.slide.goToPage(pageIndex, 0, 0)
+      this.currentPageIndex = pageIndex
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
+    deactivated() {
+      this.slide.disable()
+      clearTimeout(this.timer)
+    },
+    beforeDestroy() {
         // 当组件切换完了就调用这个生命结束周期（一般有定时器的话就习惯性要这样优化下）
-        clearTimeout(this.timer);
+      this.slide.disable()
+      clearTimeout(this.timer)
     },
-    methods:{
-        // 获取到内层滚动的轮播总长度，那么超出了外层的宽度之后才能进行横向滚动
-        _setSliderWidth(isResize){
-            // this.children获取到内部子节点数组
-            this.children = this.$refs.sliderGroup.children;
-            let sliderWidth = 0;
-            // 获取到最外层slider宽度，屏幕宽度
-            let sliderWrapWidth = this.$refs.slider.clientWidth;
-            for(let i = 0; i < this.children.length ; i++){
-                let child = this.children[i];
-                // 给内部的每个item元素加上slider-item类名
-                addClass(child,'slider-item');
-                // 设置每一个的宽度是父元素的宽度
-                child.style.width = sliderWrapWidth + 'px';
-                sliderWidth += sliderWrapWidth;
-            }
-            // 如果能够循环轮播，那么就需要左右各克隆一份，为了保证能够正常的循环轮播
-           if(!isResize){
-            if(this.loop){
-                sliderWidth += 2 * sliderWrapWidth;
-            }
-           }
-            
-
-            // 设置内部轮播的总宽度
-            this.$refs.sliderGroup.style.width = sliderWidth + 'px';
-
-        },
-        _initDots(){
-            // 初始化轮播图上面的点点
-            this.dots = new Array(this.children.length);
-        },
-        _initSlider(){
-            this.slider = new BScroll(this.$refs.slider,{
-                scrollX:true,
-                scrollY:false,
-                momentum:false,
-                // snap:true,
-                // snapLoop:this.loop,
-                // snapThreshold:0.3,
-                // snapSpeed:400,
-                click:true,
-                snap:{
-                    loop:this.loop,
-                    threshold:0.3,
-                    speed:400
-
-                }
-
-            });
-            // 能够无线轮播的滑动初始化时，滑动过程中会scrollEnd派发一个事件，在这个事件能够获取到当前是哪个轮播图
-            this.slider.on('scrollEnd', () => {
-                // 获取到当前的轮播图的index
-                let pageIndex = this.slider.getCurrentPage().pageX;
-               
-                this.currentPageIndex = pageIndex;
-                // 如果是手动拖拉轮播图，那么要清除定时器
-                if(this.autoPlay){
-                    clearTimeout(this.timer);
-                    this._autoplay();
-                }
-
-
-            });
-
-
-        },
-        _autoplay(){
-            let pageIndex = this.currentPageIndex +1;
-
-            this.timer = setTimeout(() => {
-                this.slider.goToPage(pageIndex,0,400);
-            },this.interval)
+    methods: {
+      update() {
+        if (this.slide) {
+          this.slide.destroy()
         }
+        this.$nextTick(() => {
+          this.init()
+        })
+      },
+      refresh() {
+        this._setSlideWidth(true)
+        this.slide.refresh()
+      },
+      prev() {
+        this.slide.prev()
+      },
+      next() {
+        this.slide.next()
+      },
+      init() {
+        clearTimeout(this.timer)
+        this.currentPageIndex = 0
+        this._setSlideWidth()
+        if (this.showDot) {
+          this._initDots()
+        }
+        this._initSlide()
+        if (this.autoPlay) {
+          this._play()
+        }
+      },
+      // 获取到内层滚动的轮播总长度，那么超出了外层的宽度之后才能进行横向滚动
+      _setSlideWidth(isResize) {
+          // this.children获取到内部子节点数组
+        this.children = this.$refs.slideGroup.children
+        let width = 0
+        // 获取到最外层slide宽度，屏幕宽度
+        let slideWidth = this.$refs.slide.clientWidth
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i]
+          // 给内部的每个item元素加上slide-item类名
+          addClass(child, 'slide-item')
+          child.style.width = slideWidth + 'px'
+          width += slideWidth
+        }
+        // 如果能够循环轮播，那么就需要左右各克隆一份,所以要设置多两份宽度
+        if (this.loop && !isResize) {
+          width += 2 * slideWidth
+        }
+        // 设置内部轮播的总宽度
+        this.$refs.slideGroup.style.width = width + 'px'
+      },
+      _initSlide() {
+        console.log(this.threshold)
+        this.slide = new BScroll(this.$refs.slide, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          click: this.click,
+          snap: {
+            loop: this.loop,
+            threshold: this.threshold,
+            speed: this.speed
+          }
+          // bounce: false,
+          // stopPropagation: true,
 
+        })
+        // 能够无限轮播的滑动初始化时，滑动过程中会scrollEnd派发一个事件，在这个事件能够获取到当前是哪个轮播图
+        this.slide.on('scrollEnd', this._onScrollEnd)
+        // 鼠标/手指离开触发
+        this.slide.on('touchEnd', () => {
+          // console.log(666666)
+          if (this.autoPlay) {
+            this._play()
+          }
+        })
+        this.slide.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _onScrollEnd() {
+          // 获取到当前的轮播图的index
+        let pageIndex = this.slide.getCurrentPage().pageX
+        // console.log(pageIndex)
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          clearTimeout(this.timer)
+          this._play()
+        }
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
+        // console.log(this.dots)
+      },
+      _play() {
+        clearTimeout(this.timer)
+        this.timer = setTimeout(() => {
+          this.slide.next()
+        }, this.interval)
+      }
+    },
+    watch: {
+      loop() {
+        this.update()
+      },
+      autoPlay() {
+        this.update()
+      },
+      speed() {
+        this.update()
+      },
+      threshold() {
+        this.update()
+      }
     }
-
-    
   }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-/*样式这里引入路径前面要写个 ~ 波浪线，暂时还搞不清楚*/
-  @import "~common/stylus/variable" 
-
-  .slider
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .slide
     min-height: 1px
-    .slider-group
+    .slide-group
       position: relative
       overflow: hidden
       white-space: nowrap
-      .slider-item
+      .slide-item
         float: left
         box-sizing: border-box
         overflow: hidden
@@ -182,6 +243,7 @@
       right: 0
       left: 0
       bottom: 12px
+      transform: translateZ(1px)
       text-align: center
       font-size: 0
       .dot
@@ -194,5 +256,5 @@
         &.active
           width: 20px
           border-radius: 5px
-          background: $color-text-ll
+          background: $color-text-l1
 </style>
